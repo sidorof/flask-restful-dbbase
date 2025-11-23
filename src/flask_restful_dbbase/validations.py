c1d94cf8c593262502c97356a6df36678366f512
Don Smiley <dsmiley@sidorof.com>
2025-11-23 13:53:50 -0800

    Removed logging

--- src/flask_restful_dbbase/validations.py
+++ src/flask_restful_dbbase/validations.py
@@ -2,7 +2,7 @@
 """
 Module for validating outputs.
 """
-from flask import current_app
+# from flask import current_app
 
 
 def validate_process(output, true_keys, false_keys=None):
@@ -10,29 +10,29 @@ def validate_process(output, true_keys, false_keys=None):
 
     if not isinstance(output, dict):
         msg = f"Output must a dict: {type(output)}"
-        current_app.logger.error(msg)
+        # current_app.logger.error(msg)
         raise ValueError(msg)
 
     if "status" in output:
         if not isinstance(output["status"], bool):
             msg = "'status' must be either True or False."
-            current_app.logger.error(msg)
+            # current_app.logger.error(msg)
             raise ValueError(msg)
 
         if output["status"]:
             for key in true_keys:
                 if key not in output:
                     msg = f"There must a '{key}' key in output"
-                    current_app.logger.error(msg)
+                    # current_app.logger.error(msg)
                     raise ValueError(msg)
         else:
             for key in false_keys:
                 if key not in output:
                     msg = f"There must a '{key}' key in output"
-                    current_app.logger.error(msg)
+                    # current_app.logger.error(msg)
                     raise ValueError(msg)
 
     else:
         msg = "There must a 'status' key in output"
-        current_app.logger.error(msg)
+        # current_app.logger.error(msg)
         raise ValueError(msg)
